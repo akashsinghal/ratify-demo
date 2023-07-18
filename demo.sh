3c2e3fe6ea23ca341a1775b803a16145027a07b5
Akash Singhal <akashsinghal@microsoft.com>
2023-07-17 18:41:56 -0700

    build: add more demo assets

--- demo.sh
+++ demo.sh
@@ -5,13 +5,14 @@
 run 'clear'
 
 desc "View Referrers attached to a subject image that is signed"
-run "oras discover localhost:5000/demo:signed"
+run "oras discover --plain-http localhost:5000/demo:signed"
 
 desc "View Referrers attached to a subject image that is NOT signed"
-run "oras discover localhost:5000/demo:unsigned"
+run "oras discover --plain-http localhost:5000/demo:unsigned"
 
 desc "Install Gatekeeper on local cluster"
-run ""
+run "helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts"
+run "helm install gatekeeper/gatekeeper  --name-template=gatekeeper --namespace gatekeeper-system --create-namespace --set enableExternalData=true --set validatingWebhookTimeoutSeconds=5 --set mutatingWebhookTimeoutSeconds=2"
 
 desc "Apply the Ratify constraints"
 run "kubectl apply -f template.yaml"
@@ -24,13 +25,14 @@ desc "View the Ratify constraint template"
 run "code template.yaml"
 
 desc "Install Ratify in local cluster"
-run ""
+run "helm repo add ratify https://deislabs.github.io/ratify"
+run "helm install ratify ratify/ratify --atomic --namespace gatekeeper-system --set-file notaryCert=~/.config/notation/localkeys/ratify-demo.crt --set featureFlags.RATIFY_CERT_ROTATION=true --set oras.useHttp=true"
 
 desc "Run a signed image"
-run ""
+run "kubectl run demo --image=localhost:5000/demo:signed"
 
 desc "Run an unsigned image"
-run ""
+run "kubectl run demo2 --image=localhost:5000/demo:unsigned"
 
 desc "Check the Ratify logs"
-run ""
+run "kubectl logs deployment/ratify -n gatekeeper-system"
